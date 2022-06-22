877c4559bb381f2dca25cdbcfff7877729a49ec3
okayzach <znortyy@gmail.com>
2022-06-22 12:15:10 -0400

    finished hw

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+import Loader from 'react-loader-spinner'
+
+const AboutUs = React.lazy(() => import('./components/AboutUs'))
+const Packages = React.lazy(() => import('./components/Packages'))
 
 function App() {
   return (
@@ -9,8 +11,15 @@ function App() {
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        <Suspense fallback={<Loader 
+            type="Bars" 
+            color="#FF00FF" 
+            height={200} 
+            width={200} 
+            style={{margin: "0 auto"}}/>}>
           <AboutUs />
           <Packages />
+        </Suspense>
       </div>
     </div>
   );
